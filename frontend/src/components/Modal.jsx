26d1952056c37f384a425bca2e0ccc46e3d09cfa
pratikwayase <pratikwayase@gmail.com>
2025-08-21 15:03:36 +0530

    /

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -25,7 +25,33 @@ const Modal = ({ isOpen, onClose, title, children, classname = '' }) => {
 
 
     return (
-        <div >
+        <div className={` fixed inset-0 z-50 items-center justify-center transition-colors duration-300 ${animationState === 'opening' || animationState === 'open'
+            ? 'bg-black bg-opacity-40 backdrop-blur-sm'
+            : 'bg-black bg-opacity-0'
+            }`}>
+
+            <div
+                ref={modelRef}
+                style={{ maxHeight: '90vh' }}
+                className={` bg-white dark:bg-gray-800 rounded-lg shadow-xl w-11/12 max-w-6xl onverflow-hidden flex flex-col transform transition-all duration-300 ${animationState === 'opening'
+                    ? 'scale-95 opacity-0'
+                    : animationState === 'open'
+                        ? 'scale-100 opacity-100'
+                        : 'scale-90 opacity-0'
+                    } ${classname}`}>
+
+                {/* Moal header */}
+                <div className='flex items-center justify-between p-4 border-b border-gray-200 dark:border-gray-700'>
+                    <h3 className='text-lg font-medium text-gray-800 dark:text-gray-100'>{title}</h3>
+                    <button
+                        onClick={onclose}
+                        className='p-1 rounded-full hover:bg-gay-200 dark:hover:bg-gray-700 transition-colors group'
+                        aria-label='close'>
+                        <X className='h-6 w-6 text-gray-500 dark:text-gray-400 group-hover:rotate-90 transition-transform duration-300'></X>
+                    </button>
+                </div>
+
+            </div>
 
         </div>
     )
