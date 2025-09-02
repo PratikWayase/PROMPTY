3e29cce9ec61ff0bef65ddef891658c166b227b6
pratikwayase <pratikwayase@gmail.com>
2025-09-02 15:54:01 +0530

    /

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -21,3 +21,50 @@ function getApiKey () {
     return process.env.REACT_APP_KEY || localStorage.getItem(API_KEY)
     
 }
+
+
+// get stored authentiation token 
+
+export const getToken = () => {
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+// store auth token 
+
+export const setToken = (token) => {
+    localStorage.setItem(TOKEN_KEY,token);
+
+    // set expiry for 24 hrs in future 
+
+    const expiry = Date.now() + (24 * 60 * 60 * 1000);
+    localStorage.setItem(TOKEN_EXPIRY_KEY,expiry.toString());
+
+}
+
+
+{/* remove stored token */}
+
+export const removeToken = () =>{
+    localStorage.removeItem(TOKEN_KEY)
+    localStorage.removeItem (TOKEN_EXPIRY_KEY);
+}
+
+
+{/* check if user is autheticated true if user has valid token */}
+
+export const isAuthenticated = () => {
+    const token = getToken ();
+    if  (!token) return false;
+
+    // check token exipry from our steod expiry time 
+
+    const exipry = localStorage.getItem (TOKEN_EXPIRY_KEY);
+    if (exipry && parseInt (exipry) > Date.now()){
+        return true;
+    }
+
+    // if exipry isnt set or in past token is invalid 
+
+    removeToken ();
+    return false;
+}
